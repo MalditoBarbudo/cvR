1d3fb0186f23086488aea3940b2eafb7328d6daa
MalditoBarbudo <victorgrandagarcia@gmail.com>
2015-11-21 17:04:55 +0100

    0.0.0.9002 Empty template_functions.R file
    Added an empty "template_functions.R file. It will contain the format function as well as any helper function related to the template.

---
+++ R/template_functions.R
@@ -0,0 +1,6 @@
+# Useful functions:
+#
+# join_rmds: function to join diferent rmd documents in one rmd document
+#
+# cvR_format_pdf: format function to create the output. It could make use of
+# join_rmds function to make posible a modular cv.
